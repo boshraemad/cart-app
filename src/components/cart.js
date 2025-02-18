0479ac4a0af012a7b440b6a3cb68bd253a5d9cbf
boshraemad <boshraemad2004@icloud.com>
2025-02-18 19:10:55 +0200

    handle product quantity and total price

--- src/components/cart.js
+++ src/components/cart.js
@@ -5,9 +5,15 @@ import { deleteFromCart , clearCart } from "../rtk/slices/cart-slice";
 function Cart(){
     let products=useSelector(state=>state.cart);
     let dispatch=useDispatch();
+
+    const totalPrice=products.reduce((acc,product)=>{
+        acc+=product.price*product.quantity
+        return acc;
+    },0)
     return(
        <Container className="py-5">
                 <button className="btn mt-4 btn-primary" onClick={()=>{dispatch(clearCart())}}>clear cart</button>
+                <h5 className="mt-5">total price: {totalPrice.toFixed(2)}$</h5>
                 <Table striped bordered hover className=" mt-4">
       <thead>
         <tr>
@@ -26,8 +32,8 @@ function Cart(){
                             <td>{product.id}</td>
                             <td>{product.title}</td>
                             <td><img src={product.image} style={{width:"100px" , height:"auto"}}/></td>
-                            <td>{product.price}</td>
-                            <td>1</td>
+                            <td>{product.price}$</td>
+                            <td>{product.quantity}</td>
                             <td>
                                 <button className="btn btn-danger" onClick={()=>{dispatch(deleteFromCart(product))}}>remove</button>
                                 {/* plus icon minus icon to control product quatity*/}
--- src/rtk/slices/cart-slice.js
+++ src/rtk/slices/cart-slice.js
@@ -4,7 +4,15 @@ export const cartSlice = createSlice({
     name: 'cartSlice',
     initialState:[],
     reducers: {
-        addToCart(state,action){state.push(action.payload)},
+        addToCart(state,action){
+           const findProduct= state.find((product)=>product.id===action.payload.id);
+           if(findProduct){
+                findProduct.quantity+=1;
+           }else{
+                const productsClone={...action.payload,quantity:1}//destructing
+                state.push(productsClone);
+           }
+        },
 
         deleteFromCart(state,action){
            return state.filter((product)=>product.id!==action.payload.id)
